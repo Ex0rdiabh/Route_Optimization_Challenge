8b0884f18f5f8539891c985def6b1d3bbbf95ba7
Ex0rdiabh <Ex0rdia@outlook.com>
2026-04-25 02:42:46 +0300

    Configure Vite for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/Route_Optimization_Challenge/",
   plugins: [react()],
 });
